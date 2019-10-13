811aa92292b0e1fa5fa6a1dbcd933240abca475e
Boyang Li <boyang.li@outlook.com>
2019-10-13 22:01:53 +0100

    add chap1 and chap2

---
+++ Chap_2_Matlab/.fr-jVg1HJ/hw_2/matlab版本作业/code/A_star/node_index.m
@@ -0,0 +1,11 @@
+function n_index = node_index(OPEN,xval,yval)
+    %This function returns the index of the location of a node in the list
+    %OPEN
+    %
+    %   Copyright 2009-2010 The MathWorks, Inc.
+    i=1;
+    while(OPEN(i,2) ~= xval || OPEN(i,3) ~= yval )
+        i=i+1;
+    end;
+    n_index=i;
+end
