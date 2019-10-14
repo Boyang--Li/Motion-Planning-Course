78d0822c6c945f1d4be8617c5e7b908d3e05cdb8
Boyang Li <boyang.li@outlook.com>
2019-10-14 12:07:01 +0100

    change small format

--- chap2_matlab/code/A_star_search.m
+++ chap2_matlab/code/A_star_search.m
@@ -1,35 +1,34 @@
 function path = A_star_search(map,MAX_X,MAX_Y)
-%%
-%This part is about map/obstacle/and other settings
+%%  This part is about map/obstacle/and other settings
     %pre-process the grid map, add offset
     size_map = size(map,1);
     Y_offset = 0;
     X_offset = 0;
     
     %Define the 2D grid map array.
-    %Obstacle=-1, Target = 0, Start=1
-    MAP=2*(ones(MAX_X,MAX_Y));
+    %Obstacle = -1, Target = 0, Start = 1
+    MAP = 2 * (ones(MAX_X, MAX_Y));
     
     %Initialize MAP with location of the target
-    xval=floor(map(size_map, 1)) + X_offset;
-    yval=floor(map(size_map, 2)) + Y_offset;
-    xTarget=xval;
-    yTarget=yval;
-    MAP(xval,yval)=0;
+    xval = floor(map(size_map, 1)) + X_offset;
+    yval = floor(map(size_map, 2)) + Y_offset;
+    xTarget = xval;
+    yTarget = yval;
+    MAP(xval,yval) = 0;
     
     %Initialize MAP with location of the obstacle
     for i = 2: size_map-1
-        xval=floor(map(i, 1)) + X_offset;
-        yval=floor(map(i, 2)) + Y_offset;
-        MAP(xval,yval)=-1;
+        xval = floor(map(i, 1)) + X_offset;
+        yval = floor(map(i, 2)) + Y_offset;
+        MAP(xval,yval) = -1;
     end 
     
     %Initialize MAP with location of the start point
-    xval=floor(map(1, 1)) + X_offset;
-    yval=floor(map(1, 2)) + Y_offset;
-    xStart=xval;
-    yStart=yval;
-    MAP(xval,yval)=1;
+    xval = floor(map(1, 1)) + X_offset;
+    yval = floor(map(1, 2)) + Y_offset;
+    xStart = xval;
+    yStart = yval;
+    MAP(xval,yval) = 1;
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %LISTS USED FOR ALGORITHM
@@ -38,41 +37,42 @@ function path = A_star_search(map,MAX_X,MAX_Y)
     %--------------------------------------------------------------------------
     %IS ON LIST 1/0 |X val |Y val |Parent X val |Parent Y val |h(n) |g(n)|f(n)|
     %--------------------------------------------------------------------------
-    OPEN=[];
+    OPEN = [];
+    
     %CLOSED LIST STRUCTURE
     %--------------
     %X val | Y val |
     %--------------
     % CLOSED=zeros(MAX_VAL,2);
-    CLOSED=[];
+    CLOSED = [];
 
     %Put all obstacles on the Closed list
-    k=1;%Dummy counter
+    k = 1; %Dummy counter
     for i=1:MAX_X
         for j=1:MAX_Y
             if(MAP(i,j) == -1)
-                CLOSED(k,1)=i;
-                CLOSED(k,2)=j;
-                k=k+1;
+                CLOSED(k,1) = i;
+                CLOSED(k,2) = j;
+                k = k+1;
             end
         end
     end
-    CLOSED_COUNT=size(CLOSED,1);
+    CLOSED_COUNT = size(CLOSED,1);
+    
     %set the starting node as the first node
-    xNode=xval;
-    yNode=yval;
-    OPEN_COUNT=1;
-    goal_distance=distance(xNode,yNode,xTarget,yTarget);
-    path_cost=0;
-    OPEN(OPEN_COUNT,:)=insert_open(xNode,yNode,xNode,yNode,goal_distance,path_cost,goal_distance);
-    OPEN(OPEN_COUNT,1)=0;
-    CLOSED_COUNT=CLOSED_COUNT+1;
-    CLOSED(CLOSED_COUNT,1)=xNode;
-    CLOSED(CLOSED_COUNT,2)=yNode;
-    NoPath=1;
+    xNode = xval;
+    yNode = yval;
+    OPEN_COUNT = 1;
+    goal_distance = distance(xNode,yNode,xTarget,yTarget);
+    path_cost = 0;
+    OPEN(OPEN_COUNT,:) = insert_open(xNode,yNode,xNode,yNode,goal_distance,path_cost,goal_distance);
+    OPEN(OPEN_COUNT,1) = 0;
+    CLOSED_COUNT = CLOSED_COUNT+1;
+    CLOSED(CLOSED_COUNT,1) = xNode;
+    CLOSED(CLOSED_COUNT,2) = yNode;
+    NoPath = 1;
 
-%%
-%This part is your homework
+%% This part is your homework
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % START ALGORITHM
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -93,5 +93,5 @@ function path = A_star_search(map,MAX_X,MAX_Y)
     %please finish it
     %
     
-   path = [];
+    path = [];
 end
